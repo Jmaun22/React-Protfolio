6abd633d9a5a4d735fca5089a62d22250068d776
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-06-06 21:34:29 -0400

    added tech stacks

--- your_app/src/pages/About.js
+++ your_app/src/pages/About.js
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import '../componets/css/about.css';
+import Technologies from '../componets/tech.js';
 
 
 
@@ -17,7 +18,7 @@ const Home = () => {
 
     return (
         <div className="App">
-          {/* <Particles options={particlesOptions} init={particlesInit}/> */}
+          <Particles options={particlesOptions} init={particlesInit}/>
       
            <Header/> 
             
@@ -35,12 +36,16 @@ const Home = () => {
             <p>Hi my name is Jesse I'm a Biology major who graduated from FSC.
             I have expreince with researching the mananntees microbiome and experience as a Teaching Assistant for the Biology Department.
 
-              I'm currently an aspiring web developer here are some of my projects check them out!</p>
+              I'm currently an aspiring web developer here are some of my projects check them out!
+              Hi! I'm Jesse and I recently graduated from a UCFs coding bootcamp. Before becoming a full stack web developer, I attended Dental School at LECOM. I decided to shift gears in my carreer because web development allows me to continually develop and bring amazing applications to life. It challenges my creativity in ways I didn't imagine before.
+
+In my free time I like playing disc golf, spend time with my family and enjoy the sun during an afternoon walk.</p>
       
           
                 
               
             </header>
+            <Technologies/>
         </div>
     );
 }
